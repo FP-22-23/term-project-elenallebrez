db050cb8ff0bfad3679ad1ce7f60d4d8b502a103
elenallebrez <116157269+elenallebrez@users.noreply.github.com>
2022-11-21 10:45:04 +0100

    Update module1.py

--- scr/module1.py
+++ scr/module1.py
@@ -14,3 +14,26 @@ def read_dataset(file):
         for sl_no,gender,ssc_p,hsc_p,hsc_s,degree_p,degree_t,workex, etest_p,specialisation,mba_p,status,finish_date in reader]
     
     return students
+
+#BLOQUE 1
+#Function that filters and/or selects a series of rows and/or columns of the dataset
+def hsc_p_filter(list, p=7500.0):
+    filter=[]
+    for e in list:
+        if e.hsc_p >= p:
+            filter.append(e)
+    
+    return filter
+
+#Function that calculates the sum, total or average of a numerical property
+
+def average_degree_percentage(list):
+    i=0
+    sum=0
+    for e in list:
+        sum=sum + e.degree_p
+        i=i+1
+    
+    average= (sum/i)
+
+    return average
